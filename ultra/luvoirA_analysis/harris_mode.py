47a0a707f97b035b7140686e9fb550919fc7dee7
Ananya Sahoo <asahoo@stsci.edu>
2022-12-27 17:44:58 -0500

    calculate tolerance maps

--- ultra/luvoirA_analysis/harris_mode.py
+++ ultra/luvoirA_analysis/harris_mode.py
@@ -1,7 +1,6 @@
 from astropy.io import fits
 import astropy.units as u
 import exoscene.star
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import time
@@ -10,22 +9,39 @@ from pastis.config import CONFIG_PASTIS
 from pastis.matrix_generation.matrix_from_efields import MatrixEfieldLuvoirA
 from pastis.pastis_analysis import calculate_segment_constraints
 
+from ultra.config import CONFIG_ULTRA
 from ultra.util import calculate_sensitivity_matrices
 from ultra.close_loop_analysis import req_closedloop_calc_batch
+from ultra.plotting import plot_iter_wf, plot_multimode_surface_maps
 
 if __name__ == '__main__':
 
+    # Define the type of WFE.
     WHICH_DM = 'harris_seg_mirror'
     APLC_DESIGN = 'small'
 
+    # Define target contrast
+    C_TARGET = 1e-11
+
+    # Parameters for Temporal Ananlysis
+    sptype = CONFIG_ULTRA.get('target', 'sptype')
+    Vmag = CONFIG_ULTRA.getfloat('target', 'Vmag')
+
+    minlam = CONFIG_ULTRA.getfloat('target', 'minlam') * u.nanometer
+    maxlam = CONFIG_ULTRA.getfloat('target', 'maxlam') * u.nanometer
+
+    dark_current = CONFIG_ULTRA.getfloat('detector', 'dark_current')
+    CIC = CONFIG_ULTRA.getfloat('detector', 'dark_current')
+
+    # Instantiate the simulator and run PASTIS matrix
     if WHICH_DM == 'harris_seg_mirror':
         fpath = CONFIG_PASTIS.get('LUVOIR', 'harris_data_path')  # path to Harris spreadsheet
         pad_orientations = np.pi / 2 * np.ones(CONFIG_PASTIS.getint('LUVOIR', 'nb_subapertures'))
         DM_SPEC = (fpath, pad_orientations, True, False, False)
-        NUM_MODES = 5 # TODO: works only for thermal modes currently
+        NUM_MODES = 5  # TODO: works only for thermal modes currently
 
     run_matrix = MatrixEfieldLuvoirA(which_dm=WHICH_DM, dm_spec=DM_SPEC, design=APLC_DESIGN,
-                                     calc_science=True, calc_wfs=True,
+                                     calc_science=False, calc_wfs=False,
                                      initial_path=CONFIG_PASTIS.get('local', 'local_data_path'),
                                      norm_one_photon=True)
 
@@ -33,6 +49,23 @@ if __name__ == '__main__':
     data_dir = run_matrix.overall_dir
     print(f'All saved to {data_dir}.')
 
+    tel = run_matrix.simulator
+    unaberrated_coro_psf, ref = tel.calc_psf(ref=True, display_intermediate=False, norm_one_photon=True)
+    norm = np.max(ref)
+    dh_intensity = (unaberrated_coro_psf/norm) * tel.dh_mask
+    contrast_floor = np.mean(dh_intensity[np.where(tel.dh_mask != 0)])
+
+    # contrast_floor = 4.237636070056418e-11 # past values
+    # norm = 0.010242195657579547 # past values
+
+    # Calculate static tolerances.
+    pastis_matrix = fits.getdata(os.path.join(data_dir, 'matrix_numerical', 'pastis_matrix.fits'))
+    mus = calculate_segment_constraints(pastis_matrix, c_target=C_TARGET, coronagraph_floor=0)
+    np.savetxt(os.path.join(data_dir, 'mus_harris_%s.csv' % C_TARGET), mus, delimiter=',')
+
+    plot_multimode_surface_maps(tel, mus, NUM_MODES, 'harris_seg_mirror', cmin=-5, cmax=5, data_dir=data_dir, fname=None)
+
+    # Get the efields at wfs and science plane.
     efield_science_real = fits.getdata(os.path.join(data_dir, 'matrix_numerical', 'efield_coron_real.fits'))
     efield_science_imag = fits.getdata(os.path.join(data_dir, 'matrix_numerical', 'efield_coron_imag.fits'))
     efield_wfs_real = fits.getdata(os.path.join(data_dir, 'matrix_numerical', 'efield_obwfs_real.fits'))
@@ -40,6 +73,7 @@ if __name__ == '__main__':
     ref_coron = fits.getdata(os.path.join(data_dir, 'ref_e0_coron.fits'))
     ref_obwfs = fits.getdata(os.path.join(data_dir, 'ref_e0_wfs.fits'))
 
+    # Compute sensitivity matrices.
     sensitivity_matrices = calculate_sensitivity_matrices(ref_coron, ref_obwfs, efield_science_real,
                                                           efield_science_imag,
                                                           efield_wfs_real, efield_wfs_imag, subsample_factor=8)
@@ -48,26 +82,14 @@ if __name__ == '__main__':
     e0_coron = sensitivity_matrices['ref_image_plane']
     e0_wfs = sensitivity_matrices['ref_wfs_plane']
 
-    pastis_matrix = fits.getdata(os.path.join(data_dir, 'matrix_numerical', 'pastis_matrix.fits'))
-
-    # Define target contrast
-    C_TARGET = 1e-11
-    mus = calculate_segment_constraints(pastis_matrix, c_target=C_TARGET, coronagraph_floor=0)
-    np.savetxt(os.path.join(data_dir, 'mus_harris_%s.csv' % C_TARGET), mus, delimiter=',')
-
-    tel = run_matrix.simulator
+    # Compute Star flux.
     npup = int(np.sqrt(tel.pupil_grid.x.shape[0]))
-    sptype = 'A0V'
-    Vmag = 5.0
-    minlam = 500 * u.nanometer
-    maxlam = 600 * u.nanometer
-    dark_current = 0
-    CIC = 0
-    star_flux = exoscene.star.bpgs_spectype_to_photonrate(spectype=sptype, Vmag=Vmag, minlam=minlam.value,
-                                                          maxlam=maxlam.value)
+    star_flux = exoscene.star.bpgs_spectype_to_photonrate(spectype=sptype, Vmag=Vmag,
+                                                          minlam=minlam.value, maxlam=maxlam.value)
     Nph = star_flux.value * 15 ** 2 * np.sum(tel.apodizer ** 2) / npup ** 2
     flux = Nph
-    Qharris = np.diag(np.asarray(mus ** 2))
+
+    # Set close loop parameters.
     detector_noise = 0.0
     niter = 10
     TimeMinus = -2
@@ -75,9 +97,10 @@ if __name__ == '__main__':
     Ntimes = 20
     Nwavescale = 8
     Nflux = 3
+    Qharris = np.diag(np.asarray(mus ** 2))
+
     res = np.zeros([Ntimes, Nwavescale, Nflux, 1])
     result_wf_test = []
-    norm = 0.010242195657579547
 
     for wavescale in range(1, 15, 2):
         print('recurssive close loop batch estimation and wavescale %f' % wavescale)
@@ -95,34 +118,5 @@ if __name__ == '__main__':
             n_tmp1 = len(tmp1)
             result_wf_test.append(tmp1[n_tmp1 - 1])
 
-    delta_wf = []
-    for wavescale in range(1, 15, 2):
-        wf = np.sqrt(np.mean(np.diag(0.0001 * wavescale ** 2 * Qharris))) * 1e3
-        delta_wf.append(wf)
-
-    texp = np.logspace(TimeMinus, TimePlus, Ntimes)
-    font = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 20}
-    contrast_floor = 4.237636070056418e-11
-    result_wf_test = np.asarray(result_wf_test)
-    plt.figure(figsize=(15, 10))
-    plt.title('Target contrast = %s, Vmag= %s' % (C_TARGET, Vmag), fontdict=font)
-    plt.plot(texp, result_wf_test[0:20] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[0]))
-    plt.plot(texp, result_wf_test[20:40] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[1]))
-    plt.plot(texp, result_wf_test[40:60] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[2]))
-    plt.plot(texp, result_wf_test[60:80] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[3]))
-    plt.plot(texp, result_wf_test[80:100] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[4]))
-    plt.plot(texp, result_wf_test[100:120] - contrast_floor, label=r'$\Delta_{wf}= %.2f\ pm/s$' % (delta_wf[5]))
-    plt.plot(texp, result_wf_test[120:140] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[6]))
-    plt.xlabel("$t_{WFS}$ in secs", fontsize=20)
-    plt.ylabel("$\Delta$ contrast", fontsize=20)
-    plt.yscale('log')
-    plt.xscale('log')
-    plt.legend(loc='upper center', fontsize=20)
-    plt.tick_params(top=True, bottom=True, left=True,
-                    right=True, labelleft=True, labelbottom=True,
-                    labelsize=20)
-    plt.tick_params(axis='both', which='major', length=10, width=2)
-    plt.tick_params(axis='both', which='minor', length=6, width=2)
-    plt.grid()
-    plt.savefig(os.path.join(data_dir, 'cont_wf_%s.png' % C_TARGET))
+    plot_iter_wf(Qharris, -2, 5.5, 20, result_wf_test, contrast_floor, C_TARGET, Vmag, data_dir)
 
---
+++ ultra/plotting.py
@@ -0,0 +1,93 @@
+import hcipy
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
+from pastis.util import sort_1d_mus_per_actuator
+
+
+def plot_multimode_surface_maps(tel, mus, num_modes, mirror, cmin, cmax, data_dir=None, fname=None):
+    """Creates surface deformation maps (not WFE) for localized wavefront aberrations.
+    The input mode coefficients 'mus' are in units of *WFE* and need to be grouped by segment, meaning the array holds
+    the mode coefficients as:
+        mode1 on seg1, mode2 on seg1, ..., mode'nmodes' on seg1, mode1 on seg2, mode2 on seg2 and so on.
+    Parameters:
+    -----------
+    tel : class instance of internal simulator
+        the simulator to plot the surface maps for
+    mus : 1d array
+        1d array of standard deviations for all modes on each segment, in nm WFE
+    num_modes : int
+        number of local modes used to poke each segment
+    mirror : str
+        'harris_seg_mirror' or 'seg_mirror', segmented mirror of simulator 'tel' to use for plotting
+    cmin : float
+        minimum value for colorbar
+    cmax : float
+        maximum value for colorbar
+    data_dir : str, default None
+        path to save the plots; if None, then not saved to disk
+    fname : str, default None
+        file name for surface maps saved to disk
+    """
+    if fname is None:
+        fname = f'surface_on_{mirror}'
+
+    mus_per_actuator = sort_1d_mus_per_actuator(mus, num_modes, tel.nseg)  # in nm
+
+    mu_maps = []
+    for mode in range(num_modes):
+        coeffs = mus_per_actuator[mode]
+        if mirror == 'harris_seg_mirror':
+            tel.harris_sm.actuators = coeffs * 1e-9 / 2  # in meters of surface
+            mu_maps.append(tel.harris_sm.surface)  # in m
+        if mirror == 'seg_mirror':
+            tel.sm.actuators = coeffs * 1e-9 / 2  # in meters of surface
+            mu_maps.append(tel.sm.surface)  # in m
+
+    plot_norm = TwoSlopeNorm(vcenter=0, vmin=cmin, vmax=cmax)
+    for i in range(num_modes):
+        plt.figure(figsize=(7, 5))
+        hcipy.imshow_field((mu_maps[i]) * 1e12, norm=plot_norm, cmap='RdBu')
+        plt.tick_params(top=False, bottom=True, left=True, right=False, labelleft=True, labelbottom=True)
+        cbar = plt.colorbar()
+        cbar.ax.tick_params(labelsize=10)
+        cbar.set_label("Surface (pm)", fontsize=10)
+        plt.tight_layout()
+
+        if data_dir is not None:
+            fname += f'_mode_{i}.png'
+            plt.savefig(os.path.join(data_dir, fname))
+
+
+def plot_iter_wf(Qharris, TimeMinus, TimePlus, Ntimes, result_wf_test, contrast_floor, C_TARGET, Vmag, data_dir):
+    delta_wf = []
+    for wavescale in range(1, 15, 2):
+        wf = np.sqrt(np.mean(np.diag(0.0001 * wavescale ** 2 * Qharris))) * 1e3
+        delta_wf.append(wf)
+
+    texp = np.logspace(TimeMinus, TimePlus, Ntimes)
+    font = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 20}
+
+    result_wf_test = np.asarray(result_wf_test)
+    plt.figure(figsize=(15, 10))
+    plt.title('Target contrast = %s, Vmag= %s' % (C_TARGET, Vmag), fontdict=font)
+    plt.plot(texp, result_wf_test[0:20] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[0]))
+    plt.plot(texp, result_wf_test[20:40] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[1]))
+    plt.plot(texp, result_wf_test[40:60] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[2]))
+    plt.plot(texp, result_wf_test[60:80] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[3]))
+    plt.plot(texp, result_wf_test[80:100] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[4]))
+    plt.plot(texp, result_wf_test[100:120] - contrast_floor, label=r'$\Delta_{wf}= %.2f\ pm/s$' % (delta_wf[5]))
+    plt.plot(texp, result_wf_test[120:140] - contrast_floor, label=r'$\Delta_{wf}= % .2f\ pm/s$' % (delta_wf[6]))
+    plt.xlabel("$t_{WFS}$ in secs", fontsize=20)
+    plt.ylabel("$\Delta$ contrast", fontsize=20)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.legend(loc='upper center', fontsize=20)
+    plt.tick_params(top=True, bottom=True, left=True,
+                    right=True, labelleft=True, labelbottom=True,
+                    labelsize=20)
+    plt.tick_params(axis='both', which='major', length=10, width=2)
+    plt.tick_params(axis='both', which='minor', length=6, width=2)
+    plt.grid()
+    plt.savefig(os.path.join(data_dir, 'cont_wf_%s.png' % C_TARGET))

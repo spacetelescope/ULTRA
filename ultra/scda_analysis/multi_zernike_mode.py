e1383742d613b63498eab98d9738290d5dff3168
Ananya Sahoo <asahoo@stsci.edu>
2023-09-05 18:31:05 -0400

    adapt the code to mid zernikes

--- ultra/scda_analysis/multi_zernike_mode.py
+++ ultra/scda_analysis/multi_zernike_mode.py
@@ -13,7 +13,7 @@ from pastis.util import dh_mean
 from ultra.config import CONFIG_ULTRA
 from ultra.util import calculate_sensitivity_matrices, generate_tolerance_table
 from ultra.close_loop_analysis import req_closedloop_calc_batch
-from ultra.plotting import plot_iter_wf
+from ultra.plotting import plot_iter_wf, plot_iter_mv
 
 if __name__ == '__main__':
 
@@ -42,17 +42,19 @@ if __name__ == '__main__':
     TimeMinus = CONFIG_ULTRA.getfloat('close_loop', 'TimeMinus')
     TimePlus = CONFIG_ULTRA.getfloat('close_loop', 'TimePlus')
     Ntimes = CONFIG_ULTRA.getint('close_loop', 'Ntimes')
-    Nwavescale = CONFIG_ULTRA.getfloat('close_loop', 'Nwavescale')
 
-    if WHICH_DM == 'harris_seg_mirror':
-        fpath = CONFIG_PASTIS.get('LUVOIR', 'harris_data_path')  # path to Harris spreadsheet
-        pad_orientations = np.pi / 2 * np.ones(CONFIG_PASTIS.getint('LUVOIR', 'nb_subapertures'))
-        DM_SPEC = (fpath, pad_orientations, True, False, False)
-        NUM_MODES = 7
+    wavescale_min = CONFIG_ULTRA.getint('close_loop', 'wavescale_min')
+    wavescale_max = CONFIG_ULTRA.getint('close_loop', 'wavescale_max')
+    wavescale_step = CONFIG_ULTRA.getint('close_loop', 'wavescale_step')
 
-    DM_SPEC = 7
+    mv_min = CONFIG_ULTRA.getint('close_loop', 'mv_min')
+    mv_max = CONFIG_ULTRA.getint('close_loop', 'mv_max')
+    mv_step = CONFIG_ULTRA.getint('close_loop', 'mv_step')
+
+    # Set number of Zernike modes on a segment
     NUM_MODES = 7
-    run_matrix = MatrixEfieldHex(which_dm=WHICH_DM, dm_spec=DM_SPEC, num_rings=NUM_RINGS,
+
+    run_matrix = MatrixEfieldHex(which_dm=WHICH_DM, dm_spec=NUM_MODES, num_rings=NUM_RINGS,
                                  calc_science=True, calc_wfs=True,
                                  initial_path=CONFIG_PASTIS.get('local', 'local_data_path'), norm_one_photon=True)
 
@@ -61,7 +63,6 @@ if __name__ == '__main__':
     print(f'All saved to {data_dir}.')
 
     tel = run_matrix.simulator
-
 
     unaber_psf = fits.getdata(os.path.join(data_dir, 'unaberrated_coro_psf.fits'))  # already normalized to max of direct psf
     dh_mask_shaped = tel.dh_mask.shaped
@@ -100,15 +101,12 @@ if __name__ == '__main__':
     Nph = star_flux.value * tel.diam ** 2 * np.sum(tel.apodizer ** 2) / npup ** 2
     flux = Nph
 
-    Qharris = np.diag(np.asarray(mus ** 2))
+    Q_mid_zernikes = np.diag(np.asarray(mus ** 2))
 
     unaberrated_coro_psf, ref = tel.calc_psf(ref=True, display_intermediate=False, norm_one_photon=True)
     norm = np.max(ref)
 
-    wavescale_min = 50    # TODO: plot works only for 7 wavescale values, chose the stepsize accordingly.
-    wavescale_max = 250
-    wavescale_step = 50
-    result_wf_test = []
+    contrasts_delta_wf = []
     for wavescale in range(wavescale_min, wavescale_max, wavescale_step):
         print('recurssive close loop batch estimation and wavescale %f' % wavescale)
         niter = 10
@@ -119,16 +117,37 @@ if __name__ == '__main__':
             print(tscale)
             tmp0 = req_closedloop_calc_batch(g_coron, g_wfs, e0_coron, e0_wfs, detector_noise,
                                              detector_noise, tscale, flux * Starfactor,
-                                             0.0001 * wavescale ** 2 * Qharris,
+                                             0.0001 * wavescale ** 2 * Q_mid_zernikes,
                                              niter, tel.dh_mask, norm)
             tmp1 = tmp0['averaged_hist']
             n_tmp1 = len(tmp1)
-            result_wf_test.append(tmp1[n_tmp1 - 1])
+            contrasts_delta_wf.append(tmp1[n_tmp1 - 1])
 
     np.savetxt(os.path.join(data_dir, 'contrast_wf_%s_%d_%d_%d.csv' % (C_TARGET, wavescale_min, wavescale_max, wavescale_step)),
-               result_wf_test, delimiter=',')
-    plot_iter_wf(Qharris, wavescale_min, wavescale_max, wavescale_step,
-                 TimeMinus, TimePlus, Ntimes, result_wf_test, contrast_floor, C_TARGET, Vmag, data_dir)
+               contrasts_delta_wf, delimiter=',')
+    opt_delta_contrast, opt_tscale, opt_wavescale = plot_iter_wf(Q_mid_zernikes, wavescale_min, wavescale_max, wavescale_step,
+                                                                 TimeMinus, TimePlus, Ntimes, contrasts_delta_wf,
+                                                                 contrast_floor, C_TARGET, Vmag, data_dir)
+
+    # Calculate contrast vs wavefront sensing time for different values of stellar magnitude.
+    contrasts_mv = []
+    for mv in range(mv_min, mv_max, mv_step):
+        stellar_flux = exoscene.star.bpgs_spectype_to_photonrate(spectype=sptype, Vmag=mv, minlam=minlam.value,
+                                                                 maxlam=maxlam.value)
+        entrace_flux = stellar_flux.value * tel.diam ** 2 * np.sum(tel.apodizer ** 2) / npup ** 2
+        for tscale in np.logspace(TimeMinus, TimePlus, Ntimes):
+            Starfactor = 10 ** (-StarMag / 2.5)
+            print(tscale)
+            tmp0 = req_closedloop_calc_batch(g_coron, g_wfs, e0_coron, e0_wfs, detector_noise,
+                                             detector_noise, tscale, entrace_flux * Starfactor,
+                                             0.0001 * opt_wavescale ** 2 * Q_mid_zernikes,
+                                             niter, tel.dh_mask, norm)
+            tmp1 = tmp0['averaged_hist']
+            n_tmp1 = len(tmp1)
+            contrasts_mv.append(tmp1[n_tmp1 - 1])
+
+    plot_iter_mv(contrasts_mv, mv_min, mv_max, mv_step,
+                 TimeMinus, TimePlus, Ntimes, contrast_floor, C_TARGET, data_dir)
 
     # Final Individual Tolerance allocation across 5 modes in units of pm.
     coeffs_table = np.zeros([NUM_MODES, tel.nseg])  # TODO : coeffs_table = sort_1d_mus_per_seg(mus, NUM_MODES, tel.nseg)
@@ -141,7 +160,7 @@ if __name__ == '__main__':
     opt_wavescale = 200  # This is wavescale value corresponding to local minima contrast from the graph saved above.
     opt_tscale = 0.1
 
-    Q_total = 1e3 * np.sqrt(np.mean(np.diag(0.0001 * opt_wavescale ** 2 * Qharris)))  # in pm
+    Q_total = 1e3 * np.sqrt(np.mean(np.diag(0.0001 * opt_wavescale ** 2 * Q_mid_zernikes)))  # in pm
     Q_individual = []
     for mode in range(NUM_MODES):
         Q_modes = 1e3 * np.sqrt(np.mean(0.0001 * opt_wavescale ** 2 * (coeffs_table[mode] ** 2)))  # in pm
@@ -158,15 +177,15 @@ if __name__ == '__main__':
             coeffs_tmp[qq + kk * NUM_MODES] = mus[qq + (kk) * NUM_MODES]  # arranged per modal basis
         coeffs_numaps[qq] = coeffs_tmp  # arranged into 5 groups of 600 elements and in units of nm
 
-    Qharris_individual = []
+    Q_mid_zernikes_individual = []
     for mode in range(NUM_MODES):
-        Qharris_per_mode = np.diag(np.asarray(coeffs_numaps[mode] ** 2))
-        Qharris_individual.append(Qharris_per_mode)
+        Q_mid_zernikes_per_mode = np.diag(np.asarray(coeffs_numaps[mode] ** 2))
+        Q_mid_zernikes_individual.append(Q_mid_zernikes)
 
-    Qmode = np.array(Qharris_individual)
+    Qmode = np.array(Q_mid_zernikes_individual)
 
     c_total = req_closedloop_calc_batch(g_coron, g_wfs, e0_coron, e0_wfs, detector_noise, detector_noise,
-                                        opt_tscale, flux * Starfactor, 0.0001 * opt_wavescale ** 2 * Qharris, niter,
+                                        opt_tscale, flux * Starfactor, 0.0001 * opt_wavescale ** 2 * Q_mid_zernikes, niter,
                                         tel.dh_mask, norm)
 
     resultant_c_total = []
@@ -190,6 +209,7 @@ if __name__ == '__main__':
 
     contrast_per_mode = np.array(c_per_modes)
 
-    generate_tolerance_table(tel, NUM_MODES, Q_individuals, contrast_per_mode, Q_total, contrast_floor,
-                             c0, opt_wavescale, opt_tscale, data_dir)
+    tables = generate_tolerance_table(tel, Q_individuals, Q_total,
+                                      contrast_per_mode, c0, contrast_floor, opt_wavescale, opt_tscale, data_dir)
+    print(tables[0], '\n', tables[1])
     print(f'All analysis is saved to {data_dir}.')

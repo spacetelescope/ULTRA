8aeaf50911a00c6b3487e32cf8f160d7e17a9c0e
Ananya Sahoo <asahoo@stsci.edu>
2023-10-03 20:52:08 -0400

    import util funcs n add extra comments

--- ultra/scda_analysis/harris_mode.py
+++ ultra/scda_analysis/harris_mode.py
@@ -11,14 +11,14 @@ from pastis.pastis_analysis import calculate_segment_constraints
 from pastis.util import dh_mean
 
 from ultra.config import CONFIG_ULTRA
-from ultra.util import calculate_sensitivity_matrices, generate_tolerance_table, copy_ultra_ini
+from ultra.util import calculate_sensitivity_matrices, generate_tolerance_table, copy_ultra_ini, sort_1d_mus_per_seg, sort_1d_mus_per_actuator
 from ultra.close_loop_analysis import req_closedloop_calc_batch
 from ultra.plotting import plot_iter_wf, plot_iter_mv
 
 if __name__ == '__main__':
 
     # Set number of rings
-    NUM_RINGS = 1
+    NUM_RINGS = 5
 
     # Define the type of WFE.
     WHICH_DM = 'harris_seg_mirror'
@@ -71,8 +71,9 @@ if __name__ == '__main__':
     # Call the simulator.
     tel = run_matrix.simulator
 
-    unaber_psf = fits.getdata(os.path.join(data_dir, 'unaberrated_coro_psf.fits'))   # already normalized to max(direct psf)
+    # Calculate raw coronagraphic contrast floor.
     dh_mask_shaped = tel.dh_mask.shaped
+    unaber_psf = fits.getdata(os.path.join(data_dir, 'unaberrated_coro_psf.fits'))  # already normalized to max(direct psf)
     contrast_floor = dh_mean(unaber_psf, dh_mask_shaped)
 
     # Calculate static tolerances.
@@ -112,7 +113,10 @@ if __name__ == '__main__':
     unaberrated_coro_psf, ref = tel.calc_psf(ref=True, display_intermediate=False, norm_one_photon=True)
     norm = np.max(ref)
 
-    # Calculate contrast vs wavefront sensing time for different values of Q.
+    # Calculate contrasts for different wavefront-sensor exposure times, iterating over different values of Q.
+
+    # Note: 'fractional_scale' parameter is introduced to adjust wfe-drift-scaling to float,  if required.
+    # Python range() takes only integer values.
     contrasts_delta_wf = []
     for wavescale in range(wavescale_min, wavescale_max, wavescale_step):
         print('recurssive close loop batch estimation and wavescale %f' % wavescale)
@@ -131,7 +135,7 @@ if __name__ == '__main__':
 
     np.savetxt(os.path.join(data_dir, 'contrast_wf.csv'), contrasts_delta_wf, delimiter=',')
 
-    # Calculate for optimal contrast, wfs time and drift wavefront.
+    # Get optimal contrast, wfs time and drift wavefront.
     opt_delta_contrast, opt_tscale, opt_wavescale = plot_iter_wf(Qharris, contrasts_delta_wf, contrast_floor, data_dir)
 
     print('Optimal Wavescale found:', opt_wavescale, 'Optimal wfs time scale:', opt_tscale)
@@ -140,12 +144,12 @@ if __name__ == '__main__':
     contrasts_mv = []
     for mv in range(mv_min, mv_max, mv_step):
         stellar_flux = exoscene.star.bpgs_spectype_to_photonrate(spectype=sptype, Vmag=mv, minlam=minlam.value, maxlam=maxlam.value)
-        entrace_flux = stellar_flux.value * tel.diam ** 2 * np.sum(tel.apodizer ** 2) / npup ** 2
+        entrance_flux = stellar_flux.value * tel.diam ** 2 * np.sum(tel.apodizer ** 2) / npup ** 2
         for tscale in np.logspace(TimeMinus, TimePlus, Ntimes):
             Starfactor = 10 ** (-StarMag / 2.5)
             print(tscale)
             tmp0 = req_closedloop_calc_batch(g_coron, g_wfs, e0_coron, e0_wfs, detector_noise,
-                                             detector_noise, tscale, entrace_flux * Starfactor,
+                                             detector_noise, tscale, entrance_flux * Starfactor,
                                              fractional_scale * opt_wavescale ** 2 * Qharris,
                                              niter, tel.dh_mask, norm)
             tmp1 = tmp0['averaged_hist']
@@ -157,21 +161,12 @@ if __name__ == '__main__':
     plot_iter_mv(contrasts_mv, contrast_floor, data_dir)
 
     # Final Individual Tolerance allocation across 5 modes in units of pm.
-    coeffs_table = np.zeros([NUM_MODES, tel.nseg])
-    for qq in range(NUM_MODES):
-        for kk in range(tel.nseg):
-            coeffs_table[qq, kk] = mus[qq + kk * NUM_MODES]
+    coeffs_table = sort_1d_mus_per_seg(mus, NUM_MODES, tel.nseg)   # sorts mus into NUM_MODES groups of tel.nseg
 
-    # Sort to individual modes
-    num_actuators = NUM_MODES * tel.nseg
-    coeffs_numaps = np.zeros([NUM_MODES, num_actuators])
-    for qq in range(NUM_MODES):
-        coeffs_tmp = np.zeros([num_actuators])
-        for kk in range(tel.nseg):
-            coeffs_tmp[qq + kk * NUM_MODES] = mus[qq + (kk) * NUM_MODES]  # arranged per modal basis
-        coeffs_numaps[qq] = coeffs_tmp  # arranged into NUM_MODES groups of tel.nseg * NUM_MODES elements, in nm.
+    # Sort mus into NUM_MODES groups of tel.nseg * NUM_MODES elements, in nm.
+    coeffs_numaps = sort_1d_mus_per_actuator(mus, NUM_MODES, tel.nseg)
 
-    # Calculate total delta-contrast at optimal wfs time, wavescale.
+    # Calculate total delta-contrast at optimal wfs time, optimal wavefront error scale.
     c_total = req_closedloop_calc_batch(g_coron, g_wfs, e0_coron, e0_wfs, detector_noise, detector_noise,
                                         opt_tscale, flux * Starfactor, fractional_scale * opt_wavescale ** 2 * Qharris, niter,
                                         tel.dh_mask, norm)
